d251c4f017e076e296daf95bf2a99ae010a4549b
leyes-santiago-dotcom <leyessantifacultad@gmail.com>
2026-06-15 18:45:52 -0300

    Add project files.

---
+++ d_3_tp3_Punto4/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        dtvMatriz = New DataGridView()
+        cmdEjecutar = New Button()
+        lstVector = New ListBox()
+        CType(dtvMatriz, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dtvMatriz
+        ' 
+        dtvMatriz.AllowUserToAddRows = False
+        dtvMatriz.AllowUserToDeleteRows = False
+        dtvMatriz.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvMatriz.ColumnHeadersVisible = False
+        dtvMatriz.Location = New Point(12, 47)
+        dtvMatriz.Name = "dtvMatriz"
+        dtvMatriz.ReadOnly = True
+        dtvMatriz.RowHeadersVisible = False
+        dtvMatriz.RowHeadersWidth = 51
+        dtvMatriz.Size = New Size(759, 324)
+        dtvMatriz.TabIndex = 0
+        ' 
+        ' cmdEjecutar
+        ' 
+        cmdEjecutar.Location = New Point(12, 12)
+        cmdEjecutar.Name = "cmdEjecutar"
+        cmdEjecutar.Size = New Size(94, 29)
+        cmdEjecutar.TabIndex = 1
+        cmdEjecutar.Text = "Ejecutar"
+        cmdEjecutar.UseVisualStyleBackColor = True
+        ' 
+        ' lstVector
+        ' 
+        lstVector.FormattingEnabled = True
+        lstVector.Location = New Point(777, 47)
+        lstVector.Name = "lstVector"
+        lstVector.Size = New Size(202, 324)
+        lstVector.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1050, 450)
+        Controls.Add(lstVector)
+        Controls.Add(cmdEjecutar)
+        Controls.Add(dtvMatriz)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dtvMatriz, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents dtvMatriz As DataGridView
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents lstVector As ListBox
+
+End Class
---
+++ d_3_tp3_Punto4/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    '3. Realizar un algoritmo que permita cargar una
+    'matriz de 10 por 8 elementos, y obtener un 
+    'vector de 10 elementos formado por los mínimos de cada fila. 
+
+    Dim Matriz(10, 8), Vector(10), Minimo, Indice As Integer
+
+
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Minimo = 999999
+        Indice = 1
+
+        For c = 1 To 8
+            dtvMatriz.Columns.Add("", "")
+        Next
+        dtvMatriz.Rows.Add(10)
+
+
+        For f = 1 To 10
+            For c = 1 To 8
+                Matriz(f, c) = 100 * Rnd()
+                If Matriz(f, c) < Minimo Then
+                    Minimo = Matriz(f, c)
+                End If
+            Next
+            Vector(Indice) = Minimo
+            Indice = Indice + 1
+            Minimo = 999999
+        Next
+
+        dtvMatriz.Item(0, 0).Value = "La Matriz Ingresada es"
+
+        For f = 1 To 10
+            For c = 1 To 8
+                dtvMatriz.Item(c - 1, f - 1).Value = Matriz(f, c)
+            Next
+        Next
+
+        lstVector.Items.Add("El Vector de los Mínimos es")
+        For t = 1 To 10
+            lstVector.Items.Add(Vector(t))
+        Next
+
+
+    End Sub
+End Class
